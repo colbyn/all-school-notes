2d1ff65d6121391d854b7cca34793759f6704209
Colbyn Wadman <hello@colbyn.com>
2022-10-15 22:56:57 -0600

    lets see if it works

---
+++ publish.sh
@@ -0,0 +1,11 @@
+set -e
+
+PROJECT_DIR="/Users/colbyn/Developer/all-school-notes"
+COMPILER_DIR="/Users/colbyn/Projects/subscript-root/subscript-compiler"
+OUTPUT_DIR="/Users/colbyn/Developer/all-school-notes/docs"
+
+cd $COMPILER_DIR
+
+./scripts/build.sh
+
+cargo run --release -- build --project-dir $PROJECT_DIR --route-prefix="all-school-notes" --output-dir=$OUTPUT_DIR
